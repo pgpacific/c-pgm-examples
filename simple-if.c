a09e60444bd86ed5d91b53d56ff7c9804105eb60
Prashant Joshi <prashant.joshi@leapscale.com>
2024-09-09 13:13:43 +0530

    first commit

---
+++ simple-if.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a = 100;
+    int b = 100;
+
+    // check if A is greater than B
+    if (a > b) {
+        printf("A is greater than B");
+        printf("\n");
+    }
+
+    // check if B is greater than A
+    if (b>a) {
+        printf("B is greater than A");
+        printf("\n");
+    }   
+    // check if A is equal to B
+    if (a==b){
+        printf("A is equal to B");
+         printf("\n");
+    }
+
+    return 0;
+}

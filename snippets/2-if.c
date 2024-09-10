718ef101b07fd54202ba8f4cf1ad3acb6bdaaf9f
Prashant Joshi <prashant.joshi@leapscale.com>
2024-09-10 17:58:31 +0530

    File with multicondition

---
+++ snippets/2-if.c
@@ -0,0 +1,15 @@
+// Simple IF snippet
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 15;
+    if (a == 10 && b ==15)
+    {
+        printf("A is indeed equal to %d and B is indeed equal to %d", a, b);
+        printf("\n"); // my newline
+    }
+}

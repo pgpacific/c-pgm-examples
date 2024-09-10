c6b5b2bf9f64d711a59a71e995c500786b6aeac6
Prashant Joshi <prashant.joshi@leapscale.com>
2024-09-10 18:00:33 +0530

    File with char in IF

---
+++ snippets/3-if.c
@@ -0,0 +1,14 @@
+// Simple IF snippet
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    a = 'y';
+    if (a == 'y')
+    {
+        printf("Boss gave a go ahead: %c", a);
+        printf("\n"); // my newline
+    }
+}

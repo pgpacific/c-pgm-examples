fc7548602817dfba08f8fd66b8d6ecec6673ad7d
Prashant Joshi <prashant.joshi@leapscale.com>
2024-09-10 17:54:43 +0530

    First commit for snippets

---
+++ snippets/1-if.c
@@ -0,0 +1,14 @@
+// Simple IF snippet
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = 10;
+    if (a == 10)
+    {
+        printf("A is indeed equal to %d", a);
+        printf("\n"); // my newline
+    }
+}

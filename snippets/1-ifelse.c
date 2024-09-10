777070fcb0e9f1362124867c79bac176045a87ae
Prashant Joshi <prashant.joshi@leapscale.com>
2024-09-10 18:05:34 +0530

    First IF..ELSE

---
+++ snippets/1-ifelse.c
@@ -0,0 +1,18 @@
+// Simple IF snippet
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = 10;
+    if (a == 11)
+    {
+        printf("A is indeed equal to %d", a);
+        printf("\n"); // my newline
+    }
+    else {
+        printf("A is not what I was looking for! :()");
+        printf("\n"); // my newline
+    }
+}
